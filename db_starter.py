38cd88301a2251f81b164327f048f7e3c66abe9d
deibywu <deiby.wu@maine.edu>
2023-11-17 21:38:25 -0500

    Update db_starter.py

--- db_starter.py
+++ db_starter.py
@@ -65,7 +65,7 @@ table_description_store_game = """
 CREATE TABLE IF NOT EXISTS store_game (
     store_id varchar(10) NOT NULL,
     game_id varchar(10) NOT NULL,
-    game_cost int,
+    game_cost decimal(4,2),
     FOREIGN KEY(store_id) REFERENCES store(store_id),
     FOREIGN KEY(game_id) REFERENCES games(game_id)
 )
