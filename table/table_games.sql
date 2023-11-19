c56eaf6c55078f65beab2978ecb569f154c5b46b
deibywu <deiby.wu@maine.edu>
2023-11-19 06:56:32 -0500

    Update table_games.sql

--- table/table_games.sql
+++ table/table_games.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS games (
     game_id varchar(10) NOT NULL,
     game_name varchar(250),
     release_date int,
-    num_of_players varchar(250), 
+    num_of_players int, 
     type_of_machine varchar(250),
     PRIMARY KEY(game_id)
 )
