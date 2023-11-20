b6e3144710fecd89f1f9fd4525ed11e67b8dd389
alexo75 <alexander.oneill75@gmail.com>
2023-11-20 14:47:35 -0500

    Update db_starter.py

--- db_starter.py
+++ db_starter.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from configparser import ConfigParser
 import json
 import os
 
@@ -9,15 +8,15 @@ def create_db():
 
     connection_config = config["mysql"]
     dataBase = mysql.connector.connect(host=connection_config["host"],
-    user = connection_config["user"],
-    passwd = connection_config["passwd"])
+                                       user=connection_config["user"],
+                                       passwd=connection_config["passwd"])
 
     # preparing a cursor object
     cursorObject = dataBase.cursor()
 
     # creating database
     cursorObject.execute("CREATE DATABASE IF NOT EXISTS arcade")
-
+    dataBase.close()
 
 def create_table(config_file, table_dir):
 
@@ -29,11 +28,32 @@ def create_table(config_file, table_dir):
     # preparing a cursor object
     cursor_object = data_base.cursor()
 
-    # Inserting the sql commands one by one
-    for table_sql_file in os.listdir(table_dir):
-        with open(table_dir+table_sql_file, "r") as f:
+    # List of table creation scripts ordered by dependency
+    # Hardcoded for now but we will change later
+    table_creation_order = [
+        'table_games.sql',
+        'table_game_genre.sql',
+        'table_store.sql',
+        'table_store_hours.sql',
+        'table_store_game.sql',
+        'table_user.sql',
+        # Add any other tables in the order they should be created
+    ]
+
+    # Execute the SQL files in the defined order
+    for table_sql_file in table_creation_order:
+        with open(os.path.join(table_dir, table_sql_file), "r") as f:
             table_query = f.read()
-            cursor_object.execute(table_query)
+            try:
+                cursor_object.execute(table_query)
+                data_base.commit()  # Commit changes
+            except mysql.connector.Error as err:
+                print(f"Error: '{err}' occurred while executing {table_sql_file}")
+                data_base.rollback()  # Roll back in case of error
+                break  # Stop the execution as there was an error
+
+    cursor_object.close()
+    data_base.close()
 
 # Start program here
 create_db()
