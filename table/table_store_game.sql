6c020bc6e28f81cbc85e1dadc4b7c96d3b30ab80
DeibyKwl <75148109+DeibyKwl@users.noreply.github.com>
2023-11-20 13:58:55 -0500

    Update table_store_game.sql

--- table/table_store_game.sql
+++ table/table_store_game.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS store_game (
     store_id varchar(10) NOT NULL,
     game_id varchar(10) NOT NULL,
-    game_cost decimal(4,2),
     FOREIGN KEY(store_id) REFERENCES store(store_id),
     FOREIGN KEY(game_id) REFERENCES games(game_id)
 )
