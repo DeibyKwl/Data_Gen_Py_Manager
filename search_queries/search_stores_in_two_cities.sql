b39f74f39a27d4eef9f401f6235ddade604e575e
DeibyKwl <deibykwl@gmail.com>
2023-11-20 19:23:16 -0500

    Update search_stores_in_two_cities.sql

--- search_queries/search_stores_in_two_cities.sql
+++ search_queries/search_stores_in_two_cities.sql
@@ -1,3 +1,3 @@
-(SELECT store_name FROM store WHERE city = 'portland')
+(SELECT store_name, city FROM store WHERE city = 'portland')
 UNION
-(SELECT store_name FROM store WHERE city = 'saco')
+(SELECT store_name, city FROM store WHERE city = 'saco')
