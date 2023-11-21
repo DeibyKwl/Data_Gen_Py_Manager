cb57a3542f1be6ae53eb79b6eaa375e90eef5447
DeibyKwl <deibykwl@gmail.com>
2023-11-20 19:23:31 -0500

    Update search_games_by_genre.sql

--- search_queries/search_games_by_genre.sql
+++ search_queries/search_games_by_genre.sql
@@ -1,5 +1,5 @@
 SELECT game_name FROM games
 INNER JOIN game_genre ON game_genre.game_id = games.game_id
-WHERE genre IN ('platform', 'shooter')
+WHERE genre IN ('platformer', 'shooter')
 GROUP BY game_name
 HAVING COUNT(DISTINCT(genre)) = 2;
